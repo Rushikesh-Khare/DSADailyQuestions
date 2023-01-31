033a12d09375f3fce1f1d612d1ce607a2d7a792c
Rushikesh Sunil Khare <rushikesh.khare29@gmail.com>
2023-01-31 10:31:26 +0530

    sysout added

--- Solution.java
+++ Solution.java
@@ -18,6 +18,7 @@ public class Solution {
                 me = nums[i];
                 count = 1;
             }
+            System.out.println("nothing just experimenting");
         }
         return me;
     }
